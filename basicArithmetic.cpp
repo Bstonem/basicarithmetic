3e83c9c6071bcfa406bbc7822a7a0b9886af9249
Bstonem <blystonem@gmail.com>
2025-02-15 16:28:01 -0600

    Finalized product
    Final version of program to be graded.

---
+++ basicArithmetic.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int x = 25, y = 5;
+
+    cout << x << " + " << y << " = " <<  x+y << endl;
+    cout << x << " - " << y << " = " <<  x-y << endl;
+    cout << x << " * " << y << " = " <<  x*y << endl;
+    cout << x << " / " << y << " = " <<  x/y << endl;
+    cout << x << " % " << y << " = " <<  x%y << endl;
+
+    return 0;
+
+}
